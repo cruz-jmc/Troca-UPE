08814906c5f1c3b14a161b92ea3c488dc6e158a5
João Marcelo Cruz Coelho <joao.ccoelho@upe.br>
2026-05-28 21:38:12 -0300

    feat: salvando progresso antes de testar a branch da leh

--- BackEnd/src/connection.py
+++ BackEnd/src/connection.py
@@ -1,9 +1,9 @@
 import psycopg2
-
+# Aqui vamos fazer a conexão com o bando de dados PostgreSQL usando a biblioteca psycopg2.
 def obter_conexao():
     try:
         conexao = psycopg2.connect(
-            host="localhost",
+            host="localhost", #para dar certo todas essas informações tem que ser iguais as do compose.yml
             port="5432",
             database="postgres",
             user="user",
