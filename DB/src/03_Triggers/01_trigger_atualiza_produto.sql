cd3f94357443830755d928cedf3c82e99b96d2f9
João Marcelo Cruz Coelho <joao.ccoelho@upe.br>
2026-06-03 18:29:12 -0300

    Fix: adicionei o INSERT OR UPDATE na trigger 03.

--- DB/src/03_Triggers/01_trigger_atualiza_produto.sql
+++ DB/src/03_Triggers/01_trigger_atualiza_produto.sql
@@ -12,6 +12,6 @@ $$ LANGUAGE plpgsql;
 
 -- Agora criamos a Trigger que dispara a função sempre que houver um update na tabela produtos.
 CREATE TRIGGER tg_atualiza_data_produto
-BEFORE UPDATE ON produtos -- Esse BEFORE UPDATE indica que a Trigger será executada antes da atualização dos dados.
+BEFORE INSERT OR UPDATE ON produtos -- Esse BEFORE UPDATE indica que a Trigger será executada antes da atualização dos dados.
 FOR EACH ROW -- Esse FOR EACH ROW indica que a Trigger será executada para cada linha que for atualizada.
 EXECUTE FUNCTION fn_atualizar_data_alteracao_produto();
